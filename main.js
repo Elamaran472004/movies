97d616df9601e75bb976e72044136e3396647ece
Elamaran472004 <elamaran472004@gmail.com>
2025-08-15 23:52:00 +0530

    feat: Route Structure

--- main.js
+++ main.js
@@ -7,6 +7,28 @@ app.get('/', (req,res)=>{
     res.json({msg:"this is a movies Backend API"});
 });
 
+//CRUD functionalities for movies
+
+//R - for reading the movies 
+app.get('/movies',()=>{
+
+});
+
+//C - for creating movies 
+app.post('/movies',()=>{
+
+});
+
+//U- for updating the movie
+app.put('/movies/:id',()=>{
+
+});
+
+//D- for deleting the movies 
+app.delete('/movies/:id',()=>{
+
+});
+
 app.listen(PORT,()=>{
     console.log(`server is running on port at http://localhost:${PORT}`);
 })
